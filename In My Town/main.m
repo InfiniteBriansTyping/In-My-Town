c4ee4cc78bd8e7a26c8ed5668f61ea995c9defd7
bscottk <brianscottkress@gmail.com>
2015-02-23 11:45:23 -0800

    Initial Commit

---
+++ In My Town/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  In My Town
+//
+//  Created by Brian Kress on 2/23/15.
+//  Copyright (c) 2015 Brian Kress. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
